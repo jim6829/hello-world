a9d77af3b462003e879dc469f73fad40d3d09325
Yonghoon Park <ypark6829@gmail.com>
2020-01-13 16:33:39 +0900

    add home link

--- src/components/ByeWorld.vue
+++ src/components/ByeWorld.vue
@@ -1,6 +1,7 @@
 <template>
   <div>
     <a :href="`${publicPath}files/2015-16535 박용훈_국내현장학습 계획서.hwp`" target="_self" download="sample.hwp">not found</a>
+    <router-link :to="{name: 'home'}">go home</router-link>
   </div>
 </template>
 
